8b661e28f758c6107c2464a576eaa1faecfaf32d
AnujNegi007 <anujnegi708@gmail.com>
2025-09-30 19:13:41 +0530

    add cloudinary and multer integration for image uploads

---
+++ src/lib/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "node:fs/promises";
+
+export const cloudinaryConfig = cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadImage = async (filePath) => {
+  try {
+    if (!filePath) return null;
+    const data = await cloudinary.uploader.upload(filePath);
+    await fs.unlink(filePath, {
+      resource_type: "auto",
+    });
+    console.log("Data from cloudinary", data);
+    return data.url;
+  } catch (error) {
+    await fs.unlink(filePath);
+    console.error(error);
+    return null;
+  }
+};
